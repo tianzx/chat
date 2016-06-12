8fb161ba39b6861b77bd2e90a3f0e51bb3bfbb03
tianzx <436011080@qq.com>
2016-06-12 21:27:31 +0800

    init

--- src/net/tianzx/ChatClient.java
+++ src/net/tianzx/ChatClient.java
@@ -1,6 +1,8 @@
 package net.tianzx;
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
@@ -29,6 +31,17 @@ public class ChatClient extends Frame{
                 System.exit(0);
             }
         });
+        tf.addActionListener(new TFListener());
         add(ta,BorderLayout.NORTH);
+        setVisible(true);
+    }
+
+    private class TFListener implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            String text = tf.getText().trim();
+            ta.setText(text);
+        }
     }
 }
